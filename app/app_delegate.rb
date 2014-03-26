8cc4579a8ec33537ba5a5f6f2874dd0a44b56b74
James Wagner <james@thisbythem.com>
2014-03-26 11:12:28 -0700

    Initial commit

---
+++ app/app_delegate.rb
@@ -0,0 +1,6 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    CAMediaTimingFunction.send('functionWithControlPoints::::', 0, 0, 0, 0)
+    true
+  end
+end
